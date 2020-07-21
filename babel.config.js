f6c23fed0ba1f5fad086723acc178edb53596164
huaban <2544239995@qq.com>
2020-07-21 10:47:21 +0800

    =rollback

--- babel.config.js
+++ babel.config.js
@@ -3,9 +3,5 @@ module.exports = {
     '@vue/cli-plugin-babel/preset'
   ],
   "plugins": [
-    "@babel/plugin-transform-runtime",
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/plugin-proposal-object-rest-spread",
-    "transform-vue-jsx"
   ]
 }
